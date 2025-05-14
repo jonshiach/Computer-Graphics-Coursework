044b114626824f6eded24ea6e0db04c67800b948
Ex-4 <146730152+Ex-4@users.noreply.github.com>
2025-05-14 16:03:54 +0100

    one cauldorn, diffuse specular and ambient

--- source/coursework.cpp
+++ source/coursework.cpp
@@ -91,7 +91,7 @@ int main( void )
     // Compile shader program
     unsigned int shaderID, lightShaderID;
     shaderID = LoadShaders("vertexShader.glsl", "fragmentShader.glsl");
-    //lightShaderID = LoadShaders("lightVertexShader.glsl", "lightFragmentShader.glsl");
+    lightShaderID = LoadShaders("lightVertexShader.glsl", "lightFragmentShader.glsl");
 
     // Activate shader
     glUseProgram(shaderID);
@@ -126,26 +126,6 @@ int main( void )
     glm::vec3 lightPosition = glm::vec3(2.0f, 2.0f, 2.0f);
     glm::vec3 lightColour = glm::vec3(1.0f, 1.0f, 1.0f);
 
-    //spooky cauldron positions
-    glm::vec3 positions[] = {
-        glm::vec3(0.0f,  0.0f,  0.0f),
-        glm::vec3(2.0f,  5.0f, -10.0f),
-        glm::vec3(-3.0f, -2.0f, -3.0f),
-    };
-
-
-    //add spooky cauldrons to spooky cauldrons vector
-    std::vector<Object> objects;
-    Object object;
-    object.name = "cauldron";
-    for (unsigned int i = 0; i < 10; i++)
-    {
-        object.position = positions[i];
-        object.rotation = glm::vec3(1.0f, 1.0f, 1.0f);
-        object.scale = glm::vec3(0.75f, 0.75f, 0.75f);
-        object.angle = Maths::radians(20.0f * i);
-        objects.push_back(object);
-    }
 
     // Render loop
     while (!glfwWindowShouldClose(window))
@@ -173,60 +153,20 @@ int main( void )
         camera.calculateMatrices();
 
         // Calculate the model matrix
-        //glm::mat4 translate;
-        //glm::mat4 scale;
-        //glm::mat4 rotate;
-        //glm::mat4 model = translate * rotate * scale;
+        glm::mat4 translate;
+        glm::mat4 scale;
+        glm::mat4 rotate;
+        glm::mat4 model = translate * rotate * scale;
 
-        //// Calculate the MVP matrix
-        //glm::mat4 MVP = camera.projection * camera.view * model;
-
-        //// Send MVP matrix to the vertex shader
-        //glUniformMatrix4fv(glGetUniformLocation(shaderID, "MVP"), 1, GL_FALSE, &MVP[0][0]);
-
-        //// Send MV matrix to the vertex shader
-        //glm::mat4 MV = camera.view * model;
-        //glUniformMatrix4fv(glGetUniformLocation(shaderID, "MV"), 1, GL_FALSE, &MV[0][0]);
-
-        // Loop through objects
-        for (int i = 0; i < static_cast<unsigned int>(objects.size()); i++)
-        {
-            // Calculate model matrix
-            glm::mat4 translate = Maths::translate(objects[i].position);
-            glm::mat4 scale = Maths::scale(objects[i].scale);
-            glm::mat4 rotate = Maths::rotate(objects[i].angle, objects[i].rotation);
-            glm::mat4 model = translate * rotate * scale;
-
-            // Send the MVP and MV matrices to the vertex shader
-            glm::mat4 MV = camera.view * model;
-            glm::mat4 MVP = camera.projection * MV;
-            glUniformMatrix4fv(glGetUniformLocation(shaderID, "MVP"), 1, GL_FALSE, &MVP[0][0]);
-            glUniformMatrix4fv(glGetUniformLocation(shaderID, "MV"), 1, GL_FALSE, &MV[0][0]);
-
-            // Draw the model
-            spooky_cauldron.draw(shaderID);
-        }
-
-//                          LIGHT SOURCES
-
-        glUseProgram(lightShaderID);
-
-
-        //model matrix
-        glm::mat4 translate = Maths::translate(lightPosition);
-        glm::mat4 scale = Maths::scale(glm::vec3(0.1f));
-        glm::mat4 model = translate * scale;
-
-        //send MVP & MP to vertex shader
+        // Calculate the MVP matrix
         glm::mat4 MVP = camera.projection * camera.view * model;
-        glUniformMatrix4fv(glGetUniformLocation(lightShaderID, "MVP"), 1, GL_FALSE, &MVP[0][0]);
 
-        // Send model, view, projection matrices and light colour to light shader
-        glUniform3fv(glGetUniformLocation(lightShaderID, "lightColour"), 1, &lightColour[0]);
+        // Send MVP matrix to the vertex shader
+        glUniformMatrix4fv(glGetUniformLocation(shaderID, "MVP"), 1, GL_FALSE, &MVP[0][0]);
 
-        // Draw light source
-        sphere.draw(lightShaderID);
-
+        // Send MV matrix to the vertex shader
+        glm::mat4 MV = camera.view * model;
+        glUniformMatrix4fv(glGetUniformLocation(shaderID, "MV"), 1, GL_FALSE, &MV[0][0]);
 
 
 //                          SHADER PROPERTIES
@@ -252,7 +192,7 @@ int main( void )
         glUniform1f(glGetUniformLocation(shaderID, "quadratic"), quadratic);
 
         //// Draw the witches super spooky cauldron
-        //spooky_cauldron.draw(shaderID);
+        spooky_cauldron.draw(shaderID);
 
         // Swap buffers
         glfwSwapBuffers(window);
